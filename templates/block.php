34e4aecee4410b7ee342b8e9e8a1b9485b12b13c
m-e-h <info@martyhelmick.com>
2015-05-26 12:00:47 -0400

    more styles and icon options

--- templates/block.php
+++ templates/block.php
@@ -2,23 +2,23 @@
 global $mehsc_atts;
 ?>
 
-<div class="block grid__item <?php echo esc_attr( $mehsc_atts['width'] ); ?>">
+<div class="block u-mb@respond grid__item <?php echo esc_attr( $mehsc_atts['width'] ); ?>">
     <div class="block__content">
 
         <?php if( ! empty( $mehsc_atts['icon'] ) ) : ?>
-        <div class="block__icon u-round t-bg__grey--light u-p@respond u-mt">
+        <div class="block__icon u-mhAuto u-round t-bg__grey--light u-p@respond u-mt">
             <?php get_template_part( 'images/vector/svg', esc_attr($mehsc_atts['icon'] )); ?>
         </div>
         <?php endif; ?>
 
     <?php if( ! empty( $mehsc_atts['heading'] ) ) : ?>
-    <h2 class="block__title"><?php echo esc_attr( $mehsc_atts['heading'] ); ?></h2>
+    <h2 class="block__title u-p@respond"><?php echo esc_attr( $mehsc_atts['heading'] ); ?></h2>
     <?php endif; ?>
 
-    <p class="block__body"><?php echo esc_html( $mehsc_atts['content'] ); ?></p>
+    <p class="block__body u-p@respond"><?php echo esc_html( $mehsc_atts['content'] ); ?></p>
 
         <?php if( ! empty( $mehsc_atts['footer'] ) ) : ?>
-        <div class="block__action">
+        <div class="block__action u-p@respond">
             <a href="#"><?php echo esc_attr( $mehsc_atts['footer'] ); ?></a>
         </div>
         <?php endif; ?>
